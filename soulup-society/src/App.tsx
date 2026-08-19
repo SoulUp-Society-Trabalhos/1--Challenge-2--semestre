738ba74d0aa1668b480f9e34a984a00145ab1f11
brenoell <brenobell03@gmail.com>
2026-08-19 17:01:01 -0300

    Feat: Criando futuras páginas.

--- soulup-society/src/App.tsx
+++ soulup-society/src/App.tsx
@@ -1,6 +1,8 @@
+import Home from "./pages/Home"
+
 function App() {
     return (
-        <h1>Teste bizarro</h1>
+        <Home />
     )
 }
 
---
+++ soulup-society/src/pages/Home.tsx
@@ -0,0 +1,10 @@
+function Home(){
+    return(
+        <main>
+            <h1>Teste da Home page</h1>
+            <p>Eu to com fome</p>
+        </main>
+    )
+}
+
+export default Home
