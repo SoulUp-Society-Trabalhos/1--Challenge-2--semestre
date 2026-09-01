9f600adcf55087a1ed575fe1774982b5b9943ae4
dudupizzoli <edupizzolijr@gmail.com>
2026-09-01 16:32:27 -0300

    feat : adicionando o conteúdo das páginas Proposta e Error.

--- soulup-society/src/routes/Error/index.tsx
+++ soulup-society/src/routes/Error/index.tsx
@@ -1,8 +1,19 @@
 export default function Error() {
   return (
     <main className="conteudo">
+      <section className="min-h-[90vh] bg-stone-100 font-montserrat flex flex-col items-center justify-center">
+        <div className="text-center flex flex-col gap-3">
+          <h1 className="text-6xl font-montserrat font-bold mb-6">
+            Erro 404
+          </h1>
 
-
+          <div className="text-2xl">
+            <p>
+              Página não encontrada.
+            </p>
+          </div>
+        </div>
+      </section>
     </main>
   )
   
