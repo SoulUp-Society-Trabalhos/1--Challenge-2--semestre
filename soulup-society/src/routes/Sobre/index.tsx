793df3219d10bb7143b0cca2d7851887556110a2
gustavopontes1104 <gulizpon@gmail.com>
2026-09-09 09:47:11 -0300

    feat: deixei a aba sobre mais bonita com sombra nas caixas, adicionado padding e divisao via margin

--- soulup-society/src/routes/Sobre/index.tsx
+++ soulup-society/src/routes/Sobre/index.tsx
@@ -15,30 +15,30 @@ export default function Sobre() {
         </div>
       </section>
 
-      <section className="sobre">
+      <section className="sobre bg-stone-300 px-8 py-8">
         <div className="mt-10! ml-5!">
           <p className="text-2xl text-center mb-3! bg-stone-300 p-3! w-80 mx-auto rounded-lg">
             Sobre a SoulUp Society
           </p>
-          <p className="text-base mb-1.5! mt-1.5! text-left  bg-stone-300 p-5! mr-10">
+          <p className="text-base mb-4! mt-1.5! text-left  bg-white p-5! mr-10 rounded-lg shadow-lg">
             Criada em Abril de 2026, a SoulUp Society é uma startup focada em
             aprimorar a rede social SoulUp. Realizamos e construímos nossos
             projetos mirando a implementação de novas funcionalidades no
             aplicativo, também beneficiando o meio ambiente e facilitando a
             rotina dos usuários da plataforma.
           </p>
-          <p className="text-base mb-1.5! mt-1.5! text-left bg-stone-300 p-5! mr-10">
+          <p className="text-base mb-4! mt-1.5! text-left bg-white p-5! mr-10 rounded-lg shadow-lg">
             Atualmente, nossos projetos estão centrados em questões de mobilidade, tendo como objetivo a conexão do sistema de pontos da SoulUp com os sistemas do transporte público. Além disso, também trabalhamos na adição de um sistema de missões para o aplicativo, para que os usuários consigam usufruir de uma experiência mais divertida e gratificante durante a utilização da plataforma.
           </p>
-          <p className="text-base mb-1.5! mt-1.5! text-left bg-stone-300 p-5! mr-10">
+          <p className="text-base mb-4! mt-1.5! text-left bg-white p-5! mr-10 rounded-lg shadow-lg">
             A SoulUp Society também possui ideais sustentáveis. Nossos projetos, além de gerarem uma economia para os usuários, também incentivam o uso do tarnsporte público, visando principalmente à diminuição das emissões de dióxido de carbono na atmosfera e desacelerando o efeito estufa. Acreditamos que o meio ambiente é uma parte essencial de nossa vida, portanto precisamos cuidar bem dele.
           </p>
         </div>
 
       </section>
 
-      <div className="py-8 px-[5%]! min-h-[30vh]">
-        <h2 className="text-3xl text-center font-bold border-b-4">
+      <div className="py-8 px-[5%]! min-h-[30vh] bg-stone-300">
+        <h2 className="text-3xl text-center font-bold border-b-4 border-black pb-3! mb-5!">
           Nosso Newsletter!
         </h2>
         <div className="mt-10! flex justify-center">
